9ab62accb185f192759a01346fa0397d167304dd
Olivia Mellen <omellen2269@stu.d214.org>
2021-05-27 15:21:01 -0500

    Finished project

--- src/MovingBall.java
+++ src/MovingBall.java
@@ -12,34 +12,84 @@ public class MovingBall extends JFrame {
     private int redX = 20;
     private  int redY = 180;
 
+    private int redScore = 0;
+    private int blueScore = 0;
+
+    JLabel labelR;
+    JLabel labelB;
+
     Circle blue;
+    Circle red;
 
     public MovingBall() {
         KeyListenerObject ball = new KeyListenerObject();
         addKeyListener(ball);
+
+        Container container = getContentPane();
+        container.setLayout(new FlowLayout());
+
+        labelR = new JLabel("Red score: " + redScore);
+        labelB = new JLabel("Blue score: " + blueScore);
+
+        container.add(labelR);
+        container.add(labelB);
+
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
+
+        //blue ball
         g2d.setColor(Color.blue);
         g2d.fillOval(blueX, blueY, 40,40);
         blue = new Circle(blueX + 20, blueY + 20,20);
 
+        //red ball
         g2d.setColor(Color.red);
         g2d.fillOval(redX, redY, 40,40);
+        red = new Circle(redX + 20, redY + 20, 20);
 
-        g2d.setColor(Color.black);
-        
+        //red goal
+        g.setColor(Color.black);
+        g.fillRect(0,180,10,40);
+
+        //blue goal
+        g.fillRect(590,180,10,40);
+
     }
 
+
+
     public void  checkCollisions() {
+        //if red hits blue's goal
+        if (red.intersects(590,180,10,40)) {
+            blueX = 540;
+            blueY = 180;
+            redX = 20;
+            redY = 180;
+
+            redScore ++;
+            labelR.setText("Red score: " + redScore);
+        }
+        //  if red and blue intersect
         if (blue.intersects(redX,redY,40,40)) {
             blueX = 540;
             blueY = 180;
             redX = 20;
             redY = 180;
         }
+        //if blue hits red's goal
+        if (blue.intersects(0,180,10,40)) {
+            blueX = 540;
+            blueY = 180;
+            redX = 20;
+            redY = 180;
+
+            blueScore ++;
+            labelB.setText("Blue score: " + blueScore);
+        }
+
     }
 
     private class KeyListenerObject implements KeyListener {
@@ -49,7 +99,7 @@ public class MovingBall extends JFrame {
 
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
-            int speed = 40;
+            int speed = 20;
 
             //blue down
             if (key == KeyEvent.VK_DOWN) {
@@ -99,12 +149,11 @@ public class MovingBall extends JFrame {
                     redX += speed;
                 }
             }
-            repaint();
-            checkCollisions();
         }
 
         public void keyReleased(KeyEvent e) {
             repaint();
+            checkCollisions();
         }
     }
 
